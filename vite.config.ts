781f1ee771c5e4c2436a7ea15e1591b66ccb3cdb
LivaPerfumes <Livaperfume@gmail.com>
2025-04-27 14:05:00 -0700

    CNAME added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Test3/", // Change this to match your GitHub repository name
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/", // Change this to match your GitHub repository name
+plugins: [
+  react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'CNAME', // the file we just created
+        dest: '.'     // root of the dist/ folder
+      }
+    ]
+  })
+],
+optimizeDeps: {
+  exclude: ['lucide-react'],
+}
 });
